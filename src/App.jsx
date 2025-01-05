5e7bcea8900cd3c8c85303443c5c54f071c1641b
bitDig6 <ummenabilajalal@gmail.com>
2025-01-05 12:52:00 +0600

    applied google font and created the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
 
   return (
     <>   
-      <h1 className='text-6xl'>Vite + React + Tailwind</h1>
+      <Header></Header>
     </>
   )
 }

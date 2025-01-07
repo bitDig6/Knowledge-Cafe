67ef33590e95532ccfeaac19fd1b6f2cc587a13c
bitDig6 <ummenabilajalal@gmail.com>
2025-01-07 13:06:03 +0600

    added actual image links in the json file and styled the blogs images properly

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,26 +1,30 @@
 import PropTypes from 'prop-types';
+import {FaBookmark} from 'react-icons/fa'
 
 const Blog = ({blog}) => {
     const {cover, title, author, author_img, posted_date, reading_time, hashtags} = blog;
 
     return (
         <div className='mb-20 space-y-4'>
-            <img className='w-full rounded-lg' src={cover} alt={`cover image of blog titled "${title}"`} />
+            <img className='w-full h-96 rounded-lg object-cover' src={cover} alt={`cover image of blog titled "${title}"`} />
             <div className='flex justify-between items-center'>
                 <div className='flex gap-2 items-center'>
-                <img className='rounded-full' src={author_img} alt="" />
+                <img className='w-14 rounded-full' src={author_img} alt="" />
                 <div>
                     <h5>{author}</h5>
                     <p>{posted_date}</p>
                 </div>
                 </div>
-                <p>{reading_time} min read</p>
+                <div className='flex gap-2'>
+                    <p>{reading_time} min read</p>
+                    <button className='text-red-500 text-xl'><FaBookmark></FaBookmark></button>
+                </div>
             </div>
             <h3 className='text-4xl font-bold'>{title}</h3>
             {
-                hashtags.map( (hashtag, idx) => <small key={idx}>{hashtag} </small>)
+                hashtags.map( (hashtag, idx) => <small key={idx}>#{hashtag} </small>)
             }
-            <p className='underline'><a href="#">Mark As Read</a></p>
+            <p className='underline'><a href="">Mark As Read</a></p>
         </div>
     );
 };
